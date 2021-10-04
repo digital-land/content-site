8f664176fdace437453b3b567e12d8589e90b90a
Paul Smith <mail@paulsmith.site>
2021-10-04 15:01:45 +0100

    convert netlify cms to use Javascript configuration and manual initialisation

--- etc/rollup.config.js
+++ etc/rollup.config.js
@@ -1,20 +1,35 @@
-const { nodeResolve } = require('@rollup/plugin-node-resolve')
-const commonjs = require('@rollup/plugin-commonjs')
+const { nodeResolve } = require("@rollup/plugin-node-resolve");
+const commonjs = require("@rollup/plugin-commonjs");
+import replace from "@rollup/plugin-replace";
 
-module.exports = [{
-  input: 'app/_javascripts/application.js',
-  output: {
-    file: 'public/javascripts/application.js',
-    format: 'iife'
+module.exports = [
+  {
+    input: "app/_javascripts/application.js",
+    output: {
+      file: "public/javascripts/application.js",
+      format: "iife",
+    },
+    plugins: [nodeResolve(), commonjs()],
   },
-  plugins: [
-    nodeResolve(),
-    commonjs()
-  ]
-}, {
-  input: 'node_modules/govuk-frontend/govuk/all.js',
-  output: {
-    file: 'public/javascripts/govuk-frontend.js'
+  {
+    input: "node_modules/govuk-frontend/govuk/all.js",
+    output: {
+      file: "public/javascripts/govuk-frontend.js",
+    },
+    context: "window",
   },
-  context: 'window'
-}]
+  {
+    input: "app/_javascripts/cms.js",
+    output: {
+      file: "public/javascripts/cms.js",
+    },
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      replace({
+        // this is used to set the NetlifyCMS "local_backend" configuration option at build.
+        "process.env.NODE_ENV": process.env.NODE_ENV !== "production",
+      }),
+    ],
+  },
+];
